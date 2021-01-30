da3fd1c7aba94b0f914ddf3158699ba366162306
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-01-30 13:42:11 -0800

    added program scaffolding

--- main.c
+++ main.c
@@ -1,2 +1,21 @@
+#define MAX_WORD_LENGTH 20
 
+#define MAX_NUMBER_OF_WORDS 100
 
+char *moveToBeginningOfNextToken(char *inputString);
+
+int getCurrentTokenSize(char *inputString);
+
+int tokenize(char * paragraph, char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH]);
+
+int getNumberOfWordsForNextLine(char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH], int numberOfWordsProcessedSoFar,
+                                int totalNumberOfWords, int lineLength);
+
+void printWordAndSpaces( char word[MAX_WORD_LENGTH], int numberOfSpaces);
+
+void formatAndPrintCurrentLine(char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH], int numberOfWordsProcessedSoFar,
+                               int numberOfWordsOnNextLine, int lineLength);
+
+void formatAndPrintWords(char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH], int totalNumberOfWords, int lineLength);
+
+void formatAndPrintParagraph(char *paragraph, int lineLength);
